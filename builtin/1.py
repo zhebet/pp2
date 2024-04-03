9208aecb66fe6f6624282b19e9dbea0a8ebb2754
zhebet <115343630+zhebet@users.noreply.github.com>
2024-04-04 00:05:51 +0500

    Delete builtin directory

--- builtin/1.py
+++
@@ -1,9 +0,0 @@
-def mult_list(numbers):
-    res = 1
-    for num in numbers:
-        res *= num
-    return res
-
-s = input().split(' ')
-n = [int(i) for i in s]
-print(mult_list(n))
--- builtin/3.py
+++
@@ -1,9 +0,0 @@
-def palindrome(s):
-    words = s.split(" ")
-    return s == s[::-1]
-
-s = input()
-if palindrome(s):
-    print("yes")
-else:
-    print("No")
